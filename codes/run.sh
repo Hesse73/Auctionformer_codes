67e117e969f8d5f5875b390324319ae8ef570970
LabDesktop <1457043769@qq.com>
2023-10-10 12:52:35 +0800

    default no wandb

--- codes/run.sh
+++ codes/run.sh
@@ -1,29 +1,29 @@
 # run bert
 python3 main.py --mechanisms first second --max_entry 3 --distributions uniform gaussian --start_from_zero 0 \
---iterate_game_types 1 --combine_zero 1 --pretrain_requires_grad 1 --use_wandb 1 --log_results 0 \
+--iterate_game_types 1 --combine_zero 1 --pretrain_requires_grad 1 --use_wandb 0 --log_results 0 \
 --model_name bert --gpu 0
 
 # run GPT2 (change --model_name for different models)
 python3 main.py --mechanisms first second --max_entry 3 --distributions uniform gaussian --start_from_zero 0 \
---iterate_game_types 1 --combine_zero 1 --pretrain_requires_grad 1 --use_wandb 1 --log_results 0 \
+--iterate_game_types 1 --combine_zero 1 --pretrain_requires_grad 1 --use_wandb 0 --log_results 0 \
 --model_name gpt-2 --gpu 1
 
 # run bert with no pretrain model parameter update
 python3 main.py --mechanisms first second --max_entry 3 --distributions uniform gaussian --start_from_zero 0 \
---iterate_game_types 1 --combine_zero 1 --pretrain_requires_grad 0 --use_wandb 1 --log_results 0 \
+--iterate_game_types 1 --combine_zero 1 --pretrain_requires_grad 0 --use_wandb 0 --log_results 0 \
 --model_name bert --gpu 2
 
 # run model on FP + U0 (for few-shot test)
 python3 main.py --mechanisms first --max_entry 0 --distributions uniform --start_from_zero 1 \
---iterate_game_types 1 --combine_zero 0 --pretrain_requires_grad 1 --use_wandb 1 --log_results 0 \
+--iterate_game_types 1 --combine_zero 0 --pretrain_requires_grad 1 --use_wandb 0 --log_results 0 \
 --gpu 4 --lr 1e-5
 
 # run model on FP + G0 (for few-shot test)
 python3 main.py --mechanisms first --max_entry 0 --distributions gaussian --start_from_zero 1 \
---iterate_game_types 1 --combine_zero 0 --pretrain_requires_grad 1 --use_wandb 1 --log_results 0 \
+--iterate_game_types 1 --combine_zero 0 --pretrain_requires_grad 1 --use_wandb 0 --log_results 0 \
 --gpu 5 --lr 1e-5
 
 # run model on FP + U0G0 (for few-shot test)
 python3 main.py --mechanisms first --max_entry 0 --distributions uniform gaussian --start_from_zero 1 \
---iterate_game_types 1 --combine_zero 0 --pretrain_requires_grad 1 --use_wandb 1 --log_results 0 \
+--iterate_game_types 1 --combine_zero 0 --pretrain_requires_grad 1 --use_wandb 0 --log_results 0 \
 --gpu 6 --lr 1e-5
